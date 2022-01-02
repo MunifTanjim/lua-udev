9f2a7b468f582e60bef98538a1d17ceada0aad36
Munif Tanjim <hello@muniftanjim.dev>
2022-01-02 20:40:33 +0600

    chore: add scripts/publish.sh

---
+++ scripts/publish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eu
+
+declare -r package="lua-udev"
+
+declare version="${1:-}"
+if [[ -z "${version}" ]]; then
+  echo "missing version" >&2
+  exit 1
+fi
+if [[ "${version}" != *"-"* ]]; then
+  version="${version}-1"
+fi
+shift
+
+declare -r rockspec="rockspecs/${package}-${version}.rockspec"
+
+if ! test -f ${rockspec}; then
+  echo "missing rockspec: ${rockspec}" >&2
+  exit 1
+fi
+
+declare force_upload=""
+
+if [[ "${LUAROCKS_UPLOAD_FORCE:-"false"}" = "true" ]]; then
+  force_upload="--force"
+fi
+
+luarocks upload --api-key=${LUAROCKS_API_KEY} ${force_upload} ${rockspec}
