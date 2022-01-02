e1458fd92c7c3148f25a56515617714a90ea3895
Munif Tanjim <hello@muniftanjim.dev>
2022-01-02 20:40:12 +0600

    chore: add scripts/release.sh

---
+++ scripts/release.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -eu
+
+declare -r package="lua-udev"
+
+declare version="${1:-}"
+if [[ -z "${version}" ]]; then
+  echo "missing version" >&2
+  exit 1
+fi
+if [[ "${version}" != *"-"* ]]; then
+  version="${version}-1"
+fi
+
+
+declare -r dev_rockspec="rockspecs/${package}-dev-1.rockspec"
+declare -r rockspec="rockspecs/${package}-${version}.rockspec"
+
+if test -f ${rockspec}; then
+  echo "already exists: ${rockspec}" >&2
+  exit 1
+fi
+
+cp ${dev_rockspec} ${rockspec}
+script="/^version/s|\"[^\"]\\+\"|\"${version}\"|"
+sed -e "${script}" -i ${rockspec}
+script="/^ \\+tag = nil,/s|nil|version|"
+sed -e "${script}" -i ${rockspec}
+
+luarocks make --no-install "${rockspec}"
+
+git add ${rockspec}
+
+git commit -m "chore: release ${version}"
+git tag "${version}" -m "${version}"
